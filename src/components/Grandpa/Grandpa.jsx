de314f34309ddce7b099e456e6cbe315fb360f6b
Mahafuj-Ahamed <mahafujahamed068@gamil.com>
2023-05-06 18:28:15 +0600

    create passing data using context

--- src/components/Grandpa/Grandpa.jsx
+++ src/components/Grandpa/Grandpa.jsx
@@ -4,17 +4,19 @@ import Uncle from '../Uncle/Uncle';
 import Aunty from '../Aunty/Aunty';
 import './Grandpa.css';
 
+export const RingContext = createContext('gold');
 const Grandpa = () => {
     const ring = 'Diamond';
-    const RingContext = createContext(gold);
     return (
         <div className='grandpa'>
             <h2>Grandpa</h2>
-            <section className='flex'>
-                <Father ring={ring}></Father>
-                <Uncle></Uncle>
-                <Aunty></Aunty>
-            </section>
+            <RingContext.Provider value='golden ring'>
+                <section className='flex'>
+                    <Father ring={ring}></Father>
+                    <Uncle></Uncle>
+                    <Aunty></Aunty>
+                </section>
+            </RingContext.Provider>
         </div>
     );
 };
--- src/components/Special/Special.jsx
+++ src/components/Special/Special.jsx
@@ -1,10 +1,13 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { RingContext } from '../Grandpa/Grandpa';
 
 const Special = ({ring}) => {
+    const angti = useContext(RingContext);
     return (
         <div>
             <h2>Special</h2>
-            <p><small>Ring: {ring}</small></p>
+            <p><small>Ring: {angti}</small></p>
+            
         </div>
     );
 };
